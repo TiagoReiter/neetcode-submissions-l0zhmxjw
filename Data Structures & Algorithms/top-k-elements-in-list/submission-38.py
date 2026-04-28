6509f06a94a7e68f6648cac62e8a2b1b8a658bd8
TiagoReiter <127135821+TiagoReiter@users.noreply.github.com>
2026-04-28 15:17:28 +0200

    Add: top-k-elements-in-list - submission-38

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-38.py
@@ -0,0 +1,27 @@
+"""
+TOp most frequent elements:
+1. Count the number of appearance with help of dict and get fucntion
+. COnver 
+[1 2 3 3 3], k =2
+count = 1:1, 2:1 , 3:3
+tmp = 1,1 1,2 3,3 
+
+"""
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        count = {}
+        heap = 0
+        res = []
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        temp = []
+        for num, cnt in count.items():
+            temp.append([cnt, num])
+        temp.sort()
+
+        while k > 0:
+            res.append(temp.pop()[1])
+            k -= 1
+        return res
