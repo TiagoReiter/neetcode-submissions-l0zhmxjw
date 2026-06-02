2c43d5eb5550a8077693eca108c5de6d9b917001
TiagoReiter <127135821+TiagoReiter@users.noreply.github.com>
2026-06-02 16:20:24 +0200

    Add: course-schedule - submission-10

---
+++ Data Structures & Algorithms/course-schedule/submission-10.py
@@ -0,0 +1,35 @@
+"""
+Dict in which i add all prerequisits one needs for a course
+
+1: [2, 3]
+2: [3, 4]
+"""
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        preMap = {i: [] for i in range(numCourses)}     
+
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+        
+        visiting = set()
+        def dfs(crs):
+            if crs in visiting:
+                return False
+            if preMap[crs] == []:
+                return True
+            
+            visiting.add(crs)
+
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            visiting.remove(crs)
+            preMap[crs] = []
+            return True
+        
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+        return True
